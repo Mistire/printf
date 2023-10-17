81779ce95a07d1367073746ebb85b7f97bf10c52
Mistire <mdwithgod@gamail.com>
2023-10-17 07:31:18 +0000

    printf

--- 0-printf.c
+++ 0-printf.c
@@ -1,21 +1,54 @@
 #include "main.h"
 
 /**
- * _printf - prints a formated text
- * @format: the string that needs to be formated
- * Return: the number of characters printed
+ * _printf - Custom implementation of the printf function
+ * @format: A constant pointer to the format string
+ * Return: The count of characters printed
  */
 int _printf(const char *format, ...)
 {
-	char result[1024] = "";
-	char specifier[3] = "";
-	va_list arg;
+	int (*print_func)(va_list, flags_t *);
+	const char *format_ptr;
+	va_list args;
+	flags_t flags = {0, 0, 0};
+	int char_count = 0;
 
-	if (!format)
-		return (1);
-	va_start(arg, format);
-	format_string(arg, format, specifier, result);
-	va_end(arg);
-	write(1, result, strlen(result));
-	return (strlen(result));
+	va_start(args, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+
+	for (format_ptr = format; *format_ptr; format_ptr++)
+	{
+		if (*format_ptr == '%')
+		{
+			format_ptr++;
+
+			if (*format_ptr == '%')
+			{
+				char_count += _putchar('%');
+				continue;
+			}
+
+			while (get_flag(*format_ptr, &flags))
+				format_ptr++;
+
+			print_func = get_print(*format_ptr);
+			char_count += (print_func)
+			? print_func(args, &flags)
+			: _printf("%%%c", *format_ptr);
+		}
+		else
+		{
+			char_count += _putchar(*format_ptr);
+		}
+	}
+
+	_putchar(-1);
+	va_end(args);
+	return char_count;
 }
+
