a06ca2816c25674ce6d44fc6606429f2b02cf640
root <kidusasebe1919@gmail.com>
2023-10-17 11:50:43 -0700

    correction

--- functions1.c
+++ functions1.c
@@ -106,7 +106,7 @@ int print_hexadecimal(va_list types, char buffer[],
  * Return: Number of characters printed
  */
 int print_hexa_upper(va_list types, char buffer[],
-	int flags, int width, precision, int size)
+	int flags, int width, int precision, int size)
 {
 	return (print_hexa(types, "0123456789ABCDEF", buffer,
 		flags, 'X', width, precision, size));
